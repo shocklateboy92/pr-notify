1a64a44c4382c2f1be22540a21cf290a89c0859c
Lasath Fernando <lafern@microsoft.com>
2020-03-24 17:34:21 -0700

    Add devConfig to package start script

--- src/args.ts
+++ src/args.ts
@@ -1,6 +1,7 @@
 import yargs from "yargs";
 import path from "path";
 import os from "os";
+import fs from "fs";
 
 export type IConfig = ReturnType<typeof getConfig>;
 
@@ -11,4 +12,7 @@ export const getConfig = () =>
             default: path.resolve(os.homedir(), ".cache/pr-notify.json"),
             description: "Path to file for storing app state between runs."
         })
+        .config("config", "Path to JSON configuration file", path =>
+            JSON.parse(fs.readFileSync(path, "utf-8"))
+        )
         .parse();
